a90b5e89fb6061c30e33c2ec28d3d090409c809c
Amr Mostafa Elshehaly <amshehaly@gmail.com>
2019-06-13 18:54:32 +0200

    changing results value

--- elections/src/app/candidates-elections.ts
+++ elections/src/app/candidates-elections.ts
@@ -1,7 +1,7 @@
 import {Candidate} from './candidates';
 
 export const Candidates : Candidate[] =[
-    {id:1 , name:"Tony Stark", description:"Iron Man", startDate:"10/10/2019", endDate:"10/10/2020",dateCreation:new Date(2010, 4, 18), results:30, images:"/assets/img/MF1zYXby.png" },
+    {id:1 , name:"Tony Stark", description:"Iron Man", startDate:"10/10/2019", endDate:"10/10/2020",dateCreation:new Date(2010, 4, 18), results:50, images:"/assets/img/MF1zYXby.png" },
     {id:2 , name:"Steve Rogers", description:"Captain America", startDate:"10/11/2019", endDate:"10/12/2020",dateCreation:new Date(2006, 1, 20), results:30, images:"/assets/img/130595.jpg" },
     {id:3 , name:"Thor", description:"Son of Odin", startDate:"10/11/2019", endDate:"10/12/2020",dateCreation:new Date(2006, 4, 30), results:25, images:"/assets/img/49ee773f402c73d8cd8e6720b040d220878f090a_00.jpg" },
     {id:4 , name:"Peter Parker", description:"Spiderman", startDate:"10/11/2019", endDate:"10/12/2020",dateCreation:new Date(2007, 1, 15), results:10, images:"/assets/img/dWGENKJV.jpg" },
